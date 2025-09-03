9323948fede1e0af01dd3eab4cb9bf5951cce1c4
Clara la Cour <denlacour@gmail.com>
2025-09-03 13:46:31 +0200

    yaay

--- script.js
+++ script.js
@@ -16,14 +16,26 @@ console.log(sum([10, 20, 30, 40]));
 
 //opgave 2
 
-const max = (numberArray) => {
-    let largestNumber = 0;
-    for(let i in numberArray) {
-        if (i > largestNumber) {
-            largestNumber = [i];
-        }
-    }
-    return largestNumber;
+function max(max) {
+    max.sort((a, b) => a - b);
+    return max[max.length -1];
 }
 
 console.log(max([1, 2, 3]));
+
+//opgave 3
+
+
+const countVowels = (letters) => {
+    const vowels = 'aeiouæøåAEIOUÆØÅ';
+    let count = 0; 
+
+    for(const characters of letters) {
+        if (vowels.includes(characters)) {
+            count++;
+        }
+    }
+    return count;
+};
+
+console.log(countVowels('Åge bøjede syv fine rør ud.'));
